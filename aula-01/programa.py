82274c289f2b315baf86966e2ced2fbe78ab7864
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-05-12 09:49:19 -0300

    Atualizando

--- aula-01/programa.py
+++ aula-01/programa.py
@@ -1,3 +1,8 @@
-print('Ola, Mundo!')
-print('5', '4')
-print(5 - 6)
+x = (input('Digite um número inteiro: '))
+y = (input('Digite outro número intero: '))
+
+res = 'O resultado da soma de {} com {} é {}.'.format(x, y, x + y)
+print(res)
+
+res = f'O resultado da soma de {x} com {y} é {x + y}.'
+print(res)
