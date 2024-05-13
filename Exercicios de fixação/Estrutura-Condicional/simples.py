e753508a0cfe490f7291bfeead2ec9c7d8228d53
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-05-13 06:46:12 -0300

    Atualizando

---
+++ Exercicios de fixação/Estrutura-Condicional/simples.py
@@ -0,0 +1,17 @@
+#Lê dois valores inteiros e compara ambos
+# x = int(input('Digite um valor interio: '))
+# y = int(input('Digite um segundo valor inteiro: '))
+
+
+# if( x > y ) :
+#     print('O primeiro valor é maior que o segundo!')
+
+
+    #Lê dois valores e compara ambos
+x = int(input('Digite um valor interio: '))
+y = int(input('Digite um segundo valor inteiro: '))
+
+if (x > y ) :
+    print('O primeiro valor é maior que o segundo!')
+if (x < y ) : 
+    print('O segundo valor é maior que o primeiro!')    
