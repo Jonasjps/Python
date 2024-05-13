b3993e749f7c3758dccbede4cd6a2128cd2237d1
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-05-13 07:36:59 -0300

    Atualizando ex

---
+++ Exercicios de fixação/Estrutura-Condicional/condição-elif.py
@@ -0,0 +1,19 @@
+print('Escolha o que deseja comprar: ')
+print('1 - Maça')
+print('2 - Laranja')
+print('3 - Banana')
+
+produto = int(input('Qual sua escolha?'))
+qtd = int(input('quantas unidades?'))
+
+if (produto == 1 ) : 
+    pagar = qtd * 2.3
+    print(f'Você comprou {qtd} maças. Total a pagar: {pagar}')
+elif (produto == 2) : 
+    pagar = qtd * 3.6
+    print(f'Você comprou {qtd} maças. Total a pagar: {pagar}')
+elif (produto == 3) :
+    pagar = qtd * 1.85
+    print(f'Você comprou {qtd} maças. Total a pagar: {pagar}')
+else :
+    print('Produto inexistente')
