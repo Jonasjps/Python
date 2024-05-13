291cef37b858bba0b105796eee8c484426ad9df3
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-05-13 06:58:39 -0300

    AStualizando

---
+++ Exercicios de fixação/Estrutura-Condicional/composta.py
@@ -0,0 +1,16 @@
+  #Lê dois valores e compara ambos
+# x = int(input('Digite um valor interio: '))
+# y = int(input('Digite um segundo valor inteiro: '))
+
+# if (x > y ) :
+#     print('O primeiro valor é maior que o segundo!')
+# else : 
+#     print('O segundo valor é maior que o primeiro!')    
+
+    #Par ou impar
+x = int(input('Digite um valor inteiro: '))
+
+if(x % 2 == 0) : 
+    print('O numero é Par')
+else :
+    print('O numero é impar')
