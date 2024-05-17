294d1249c09a4bee1678da590e04e1b8966606fd
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-05-17 06:47:16 -0300

    Atualizando

--- Exercicios de fixação/Estrutura-Condicional/ex-04.py
+++ Exercicios de fixação/Estrutura-Condicional/ex-04.py
@@ -1,7 +1,17 @@
 # Validando a entrada 
 
-x = int(input('Digite um valor maior do que zero: '))
+# x = int(input('Digite um valor maior do que zero: '))
 
-while (x <= 0) : 
-    x = int(input('Digite um valor maior do que zero: '))
-print(f'Vacê digitou {x}. Encerrando o programa...')
+# while (x <= 0) : 
+#     x = int(input('Digite um valor maior do que zero: '))
+# print(f'Vacê digitou {x}. Encerrando o programa...')
+
+
+soma = 0 
+qtd = 0
+for i in range(1, 101): 
+    if (i % 2 == 0 ):
+        soma += i
+        qtd += 1
+media = soma / qtd
+print(f'A media dos pares de 1 até 100 é: {media}')
