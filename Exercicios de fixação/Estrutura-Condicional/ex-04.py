4e3c7483719b80f1876c497c9e1858f9123fed3a
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-05-19 12:46:04 -0300

    Atualizando

--- Exercicios de fixação/Estrutura-Condicional/ex-04.py
+++ Exercicios de fixação/Estrutura-Condicional/ex-04.py
@@ -7,11 +7,28 @@
 # print(f'Vacê digitou {x}. Encerrando o programa...')
 
 
-soma = 0 
-qtd = 0                                  
-for i in range(1, 101): 
-    if (i % 2 == 0 ):
-        soma += i
-        qtd += 1
-media = soma / qtd
-print(f'A media dos pares de 1 até 100 é: {media}')
+# soma = 0 
+# qtd = 0                                  
+# for i in range(1, 101): 
+#     if (i % 2 == 0 ):
+#         soma += i
+#         qtd += 1
+# media = soma / qtd
+# print(f'A media dos pares de 1 até 100 é: {media}')
+
+# count = 5
+# soma = 0
+# while count <= 25:
+#     soma = soma + count
+#     count = count + 5
+# print(soma)
+
+# for i in range (10, 20) :
+#     for j in range (10, 20, 2) :
+#         print('{} + {} = {}'.format(i, j, i + j))
+
+x = int(input('Digite um valor no intervalo de -100 até 100: '))
+
+while (x < 99 and x > -99) : 
+    x = int(input('Digite um valor no intervalo de -100 até 100: '))
+print('Encerrado...')
