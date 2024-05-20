35d36e5cb6455d4f1c43e593f35ae6af4c48de74
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-05-19 21:05:52 -0300

    Atualizando

--- trabalho/ex-01.py
+++ trabalho/ex-01.py
@@ -9,28 +9,28 @@ if valor_total < 2500 : # verifica se o valor da compra é menor que 2500.
 
     desconto = valor_total - (valor_total * 0 /100) # calcula o valor da compra já com o desconto.
 
-    print(f'O valor SEM desconto: {valor_total}') #valor COM desconto 
-    print(f'O valor COM desconto: {desconto} {' ' * 10} valor total sem desconto, para obter o desconto inicial de 4% é preciso que a compra \n {' '* 77}seja maior ou igual a 2500 para mais descontos. ')    #valor SEM desconto
+    print(f'O valor SEM desconto: {valor_total}') #valor SEM desconto 
+    print(f'O valor COM desconto: {desconto} {" " * 10} Desconto de 0% aplicado para valor total menor que 2500. \n {" "*43}Para obter descontos faça uma compra maior que 2500. ')    #valor COM desconto
 
 elif valor_total >= 2500 and valor_produto < 6000: #verica se o valor da compara é maior ou igual a 2500 e menor que 6000
 
     desconto = valor_total - (valor_total * 4 / 100) # calcula o valor da compra já com o desconto.
 
-    print(f'O valor SEM desconto: {valor_total}')  #valor COM desconto
-    print(f'O valor COM desconto: {desconto}')     #valor SEM desconto
+    print(f'O valor SEM desconto: {valor_total}')  #valor SEM desconto
+    print(f'O valor COM desconto: {desconto} {" "*10} Desconto de 4% aplicado para valor total igual ou maior que 2500 e menor que 6000') #valor COM desconto
 
-elif  valor_total >= 6000 and valor_produto < 10000 : # verifica se o valor da compra é maior ou igual a 6000 e menor que 10000
+elif valor_total >= 6000 and valor_produto < 10000 : # verifica se o valor da compra é maior ou igual a 6000 e menor que 10000
 
     desconto = valor_total - (valor_total * 7 / 100) # calcula o valor da compra já com o desconto.
 
-    print(f'O valor SEM desconto: {valor_total}')  #valor COM desconto
-    print(f'O valor COM desconto: {desconto}')     #valor SEM desconto
+    print(f'O valor SEM desconto: {valor_total}')  #valor SEM desconto
+    print(f'O valor COM desconto: {desconto} {" "*10} Desconto de 7% aplicado para valor total igual ou maior que 6000 e menor que 10000') #valor COM desconto
 
 else : # nesse bloco, vai ser verificado se o valor da compra é igual ou maior que 10000.
 
     desconto = valor_total - (valor_total * 11 / 100) # calcula o valor da compra já com o desconto.
 
-    print(f'O valor SEM desconto: {valor_total}')  #valor COM desconto
-    print(f'O valor COM desconto: {desconto}')     #valor SEM desconto
+    print(f'O valor SEM desconto: {valor_total}')  #valor SEM desconto
+    print(f'O valor COM desconto: {desconto}  {" "*10} Desconto de 11% aplicado para valor total igual ou maior que 10000') #valor COM desconto
 
 
