cc428347f3d856a8c2c5cce3571acbe491fe69c6
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-05-23 00:06:26 -0300

    Atualizandp

--- trabalho/ex-03.py
+++ trabalho/ex-03.py
@@ -1,7 +1,18 @@
 print('Bem vindo a Copiadora do Jonas Pessoa.\n')
 
-def escolha_servico ():
-  print('Entre com o tipo de serviço desejado ')
-  print('DIG - Digitação\nICO - Impressão Colorida\nIPB - Impressão Preto e Branco\nFOT - Fotocópia')
+while True:
+  def escolha_servico ():
+    print('Entre com o tipo de serviço desejado ')
+    print('DIG - Digitação\nICO - Impressão Colorida\nIPB - Impressão Preto e Branco\nFOT - Fotocópia')
+    servico = input('>>')
 
-escolha_servico()
+    if(servico == 'DIG'.lower() or servico == 'ICO'.lower() or servico == 'IPB'.lower() or servico == 'FOT'.lower()):
+        num_paginas = print(int(input('Entre com o número de páginas:')))
+      
+      
+    if(servico != 'DIG'.lower() or servico != 'ICO'.lower() or servico != 'IPB'.lower() or servico != 'FOT'.lower()):
+      print('Escolha inválida, entre com o tipo do serviço novamente.')
+
+      
+  escolha_servico()
+  continue
