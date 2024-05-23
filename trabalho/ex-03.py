36068993c304c3fcc045aea97545f666bbd98c84
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-05-22 23:42:42 -0300

    Atua

---
+++ trabalho/ex-03.py
@@ -0,0 +1,7 @@
+print('Bem vindo a Copiadora do Jonas Pessoa.\n')
+
+def escolha_servico ():
+  print('Entre com o tipo de serviço desejado ')
+  print('DIG - Digitação\nICO - Impressão Colorida\nIPB - Impressão Preto e Branco\nFOT - Fotocópia')
+
+escolha_servico()
