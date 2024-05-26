d9d3917bca87238fe4380d4190345ae607db3de8
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-05-26 08:25:44 -0300

    Atualizando app

--- trabalho/ex-03.py
+++ trabalho/ex-03.py
@@ -6,13 +6,13 @@ while True:
     print('DIG - Digitação\nICO - Impressão Colorida\nIPB - Impressão Preto e Branco\nFOT - Fotocópia')
     servico = input('>>')
 
-    if(servico == 'DIG'.lower() or servico == 'ICO'.lower() or servico == 'IPB'.lower() or servico == 'FOT'.lower()):
-        num_paginas = print(int(input('Entre com o número de páginas:')))
-      
       
     if(servico != 'DIG'.lower() or servico != 'ICO'.lower() or servico != 'IPB'.lower() or servico != 'FOT'.lower()):
       print('Escolha inválida, entre com o tipo do serviço novamente.')
 
+    if(servico == 'DIG'.lower() or servico == 'ICO'.lower() or servico == 'IPB'.lower() or servico == 'FOT'.lower()):
+      num_paginas = print(int(input('Entre com o número de páginas:')))
+      break
       
   escolha_servico()
   continue
